b440639c5a9087e70622fbd113cc5b2b7ab11186
PATTEM BHANU PRAKASH <bhanupattemz@gmail.com>
2025-02-23 00:39:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+   "rewrites": [
+    {
+      "source": "/(.*)",
+      "destination": "/index.html"
+    }
+  ]
 })
